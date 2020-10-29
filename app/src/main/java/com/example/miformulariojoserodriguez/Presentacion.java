5392aee20b97d458133d5fdeb525703a6f75ea8f
Jose Rodriguez <jrodriguezballester@gmail.com>
2020-10-29 13:37:37 +0100

    estructura tres activitys finalizada

--- app/src/main/java/com/example/miformulariojoserodriguez/Presentacion.java
+++ app/src/main/java/com/example/miformulariojoserodriguez/Presentacion.java
@@ -8,7 +8,10 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Presentacion extends AppCompatActivity {
 
@@ -25,7 +28,21 @@ public class Presentacion extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+        Button btn = (Button) findViewById(R.id.btn_toast_pre);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast toast2 =
+                        Toast.makeText(getApplicationContext(),
+                                "Toast con gravity", Toast.LENGTH_SHORT);
+                toast2.setGravity(Gravity.CENTER|Gravity.LEFT,0,0);
+
+                toast2.show();
+            }
+        });
+
     }
 }
